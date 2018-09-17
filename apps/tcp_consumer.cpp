579e227a13945864d69e0e03eb25134f9b0f421a
Jonathan Herbst <amd64d@gmail.com>
2018-09-16 23:00:27 -0400

    More work on the tcp consumer (untested)

--- apps/tcp_consumer.cpp
+++ apps/tcp_consumer.cpp
@@ -3,20 +3,20 @@
 #include <shmbus/producer.hpp>
 #include <shmbus/consumer.hpp>
 
+#include <memory>
 #include <chrono>
 #include <iostream>
+#include <list>
 
 namespace asio = boost::asio;
 
-class TCPClient
+class TCPClient : public std::enable_shared_from_this<TCPClient>
 {
 public:
-    TCPClient(asio::io_service& ioService, shmbus::producer& rxBus, shmbus::consumer& txBus) :
+    TCPClient(asio::io_service& ioService, shmbus::producer& rxBus) :
     m_socket(ioService),
     m_timer(ioService),
-    m_rxBus(rxBus),
-    m_txBus(txBus),
-    m_bufferPtr(asio::buffer(m_buffer.data(), m_buffer.size()))
+    m_rxBus(rxBus)
     {}
 
     asio::ip::tcp::socket& socket()
@@ -24,8 +24,9 @@ public:
         return m_socket;
     }
 
-    void run()
+    void run(const std::string& txBusName)
     {
+        m_txBus = std::make_unique<shmbus::consumer>(shmbus::open, txBusName);
         receive();
         transmitOrWait();
     }
@@ -34,7 +35,7 @@ private:
     asio::ip::tcp::socket m_socket;
     asio::steady_timer m_timer;
     shmbus::producer& m_rxBus;
-    shmbus::consumer& m_txBus;
+    std::unique_ptr<shmbus::consumer> m_txBus;
     std::array<uint8_t, 4096> m_buffer;
     asio::mutable_buffer m_bufferPtr;
 
@@ -46,7 +47,7 @@ private:
             if(bytesWritten < bytesTransmitted)
             {
                 m_bufferPtr = m_bufferPtr + bytesWritten;
-                m_socket.get_io_service().post(std::bind(&TCPClient::rxCallback, this, error, bytesTransmitted - bytesWritten));
+                m_socket.get_io_service().post(std::bind(&TCPClient::rxCallback, shared_from_this(), error, bytesTransmitted - bytesWritten));
             }
             else
             {
@@ -59,7 +60,7 @@ private:
     {
         if(not error)
         {
-            m_txBus.consume(bytesTransmitted);
+            m_txBus->consume(bytesTransmitted);
             transmitOrWait();
         }
     }
@@ -76,34 +77,121 @@ private:
     {
         const void* data;
         unsigned int dataSize;
-        std::tie(data, dataSize) = m_txBus.data();
+        std::tie(data, dataSize) = m_txBus->data();
 
         if(dataSize > 0)
         {
-            m_socket.async_write_some(asio::buffer(data, dataSize), std::bind(&TCPClient::txCallback, this, asio::placeholders::error, asio::placeholders::bytes_transferred));
+            m_socket.async_write_some(asio::buffer(data, dataSize), std::bind(&TCPClient::txCallback, shared_from_this(), asio::placeholders::error, asio::placeholders::bytes_transferred));
         }
         else
         {
             m_timer.expires_after(std::chrono::milliseconds(10));
-            m_timer.async_wait(std::bind(&TCPClient::timeout, this, asio::placeholders::error));
+            m_timer.async_wait(std::bind(&TCPClient::timeout, shared_from_this(), asio::placeholders::error));
         }
     }
 
     void receive()
     {
         m_bufferPtr = asio::buffer(m_buffer.data(), m_buffer.size());
-        m_socket.async_read_some(m_bufferPtr, std::bind(&TCPClient::rxCallback, this, asio::placeholders::error, asio::placeholders::bytes_transferred));
+        m_socket.async_read_some(m_bufferPtr, std::bind(&TCPClient::rxCallback, shared_from_this(), asio::placeholders::error, asio::placeholders::bytes_transferred));
     }
 };
 
 class TCPServer
 {
 public:
-    TCPServer(asio::io_service& ioService, shmbus::producer& rxBus, shmbus::consumer& txBus, uint16_t port, unsigned int maxConnections);
+    TCPServer(asio::io_service& ioService, shmbus::producer& rxBus, const std::string& txBusName, uint16_t port, unsigned int maxConnections) :
+    m_v4Endpoint(asio::ip::tcp::v4(), port),
+    m_v6Endpoint(asio::ip::tcp::v6(), port),
+    m_v4Acceptor(ioService, m_v4Endpoint),
+    m_v6Acceptor(ioService, m_v6Endpoint),
+    m_rxBus(rxBus),
+    m_txBusName(txBusName),
+    m_maxClients(maxConnections)
+    {
+        m_v4Acceptor.listen(10);
+        acceptV4();
+        m_v6Acceptor.listen(10);
+        acceptV6();
+    }
 
 private:
-    asio::ip::tcp::acceptor m_acceptor;
-    shmbus::producer& rxBus;
-    shmbus::consumer& txBus;
-    std::list<std::shared_ptr<TCPClient>> m_connections;
+    asio::ip::tcp::endpoint m_v4Endpoint;
+    asio::ip::tcp::endpoint m_v6Endpoint;
+    asio::ip::tcp::acceptor m_v4Acceptor;
+    asio::ip::tcp::acceptor m_v6Acceptor;
+    shmbus::producer& m_rxBus;
+    std::string m_txBusName;
+    std::shared_ptr<TCPClient> m_nextV4Client;
+    std::shared_ptr<TCPClient> m_nextV6Client;
+    std::list<std::weak_ptr<TCPClient>> m_clients;
+    unsigned int m_maxClients;
+
+    void cleanClients()
+    {
+        m_clients.remove_if([](std::weak_ptr<TCPClient> client){return client.expired();});
+    }
+
+    void acceptV4Callback(const boost::system::error_code& error)
+    {
+        if(not error)
+        {
+            cleanClients();
+            if(m_clients.size() < m_maxClients)
+            {
+                m_clients.emplace_back(m_nextV4Client);
+                m_nextV4Client->run(m_txBusName);
+            }
+            acceptV4();
+        }
+    }
+
+    void acceptV6Callback(const boost::system::error_code& error)
+    {
+        if(not error)
+        {
+            cleanClients();
+            if(m_clients.size() < m_maxClients)
+            {
+                m_clients.emplace_back(m_nextV6Client);
+                m_nextV6Client->run(m_txBusName);
+            }
+            acceptV6();
+        }
+    }
+
+    void acceptV4()
+    {
+        m_nextV4Client.reset(new TCPClient(m_v4Acceptor.get_io_service(), m_rxBus));
+        m_v4Acceptor.async_accept(m_nextV4Client->socket(), std::bind(&TCPServer::acceptV4Callback, this, asio::placeholders::error));
+    }
+
+    void acceptV6()
+    {
+        m_nextV6Client.reset(new TCPClient(m_v6Acceptor.get_io_service(), m_rxBus));
+        m_v6Acceptor.async_accept(m_nextV6Client->socket(), std::bind(&TCPServer::acceptV6Callback, this, asio::placeholders::error));
+    }
 };
+
+int main(int argc, char* argv[])
+{
+    if(argc != 4)
+    {
+        std::cout << "Usage: " << std::string(argv[0]) << " <bus_name> <port> <max_connections>" << std::endl;
+        return 1;
+    }
+
+    std::string busName(argv[1]);
+    uint16_t port = std::stoul(std::string(argv[2]));
+    unsigned int maxConnections = std::stoul(std::string(argv[3]));
+
+    std::string rxBusName = busName + "_rx";
+    shmbus::producer rxBus(shmbus::open, rxBusName);
+
+    std::string txBusName = busName + "_tx";
+
+    asio::io_service ioService;
+    TCPServer server(ioService, rxBus, txBusName, port, maxConnections);
+
+    ioService.run();
+}
